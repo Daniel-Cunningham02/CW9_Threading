6190e41e3277d7e9e75651794c4d734e293367ca
Daniel Cunningham <danrcunn@vols.utk.edu>
2023-03-02 12:37:52 -0600

    Add project files.

---
+++ Threading/FindPiThread.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    internal class FindPiThread
+    {
+        int numDarts;
+        int numDartsInTarget;
+        Random random;
+
+
+        public int numDartsHit {
+            get { return numDarts; }
+        }
+        public FindPiThread(int numDarts)
+        {
+            this.numDarts = numDarts;
+            random = new Random();
+            numDartsInTarget = 0;
+        }
+
+
+        public void throwDarts()
+        {
+            for(int i = 0; i < numDarts; i++)
+            {
+                int x = random.Next(-1, 1);
+                int y = random.Next(-1, 1);
+                if (x > 0 && y > 0)
+                {
+                    numDartsInTarget++;
+                }
+            }
+        }
+    }
+}
---
+++ Threading/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.Write("How many throws would you like for each thread? ");
+            int amountDarts = Int32.Parse(Console.ReadLine());
+            Console.Write("How many thread would you like to simulate? ");
+            int amountThreads = Int32.Parse(Console.ReadLine());
+            List<Thread> threads = new List<Thread>();
+            List<FindPiThread> findPiThreads = new List<FindPiThread>();
+            for(int i = 0; i < amountThreads; i++)
+            {
+                findPiThreads.Add(new FindPiThread(amountDarts));
+                threads.Add(new Thread(new ThreadStart(findPiThreads[i].throwDarts)));
+                threads[i].Start();
+                Thread.Sleep(16);
+            }
+
+            foreach(Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            foreach (FindPiThread i in findPiThreads) {
+                sum += i.numDartsHit;
+            }
+
+            double estimation_of_pi = (4 * sum) / amountDarts;
+            Console.WriteLine(estimation_of_pi.ToString());
+        }
+    }
+}
